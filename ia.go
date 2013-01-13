e99ed9fb575190b2fb05c207fc16a5e0fd67c978
Alexandre Baron <alexandre.baron@epitech.eu>
2013-01-13 18:04:21 +0100

    Add de ia.go

---
+++ ia.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func gameStateValue(game *Gomoku) int {
+	return 42
+	
+}
+
+func IATurn(game *Gomoku) (int, int) {
+	fmt.Println("Bon c'est au tour de l'IA la")
+	return 42, 42
+}
