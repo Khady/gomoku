45719b81d7978aa59b6d7d36655ce7f0bb1734f9
Alexandre Baron <alexandre.baron@epitech.eu>
2013-01-19 11:44:07 +0100

    debut de reseau

--- newIA/game.go
+++ newIA/game.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -12,12 +13,24 @@ type GomokuBoard struct {
 	DRLLines    [37]uint64
 	endgameTake bool
 	doubleThree bool
+	playerTurn  uint64
+	stonesTaken [2]byte
+	DoubleThreeCheckFuncs map[uint8]func (int, int) bool
 }
 
 const (
 	EMPTY = 0
 	WHITE = 1
 	BLACK = 2
+
+	UP        = 0x1
+	LEFT      = 0x2
+	DOWN      = 0x4
+	RIGHT     = 0x8
+	LEFTUP    = 0x10
+	LEFTDOWN  = 0x20
+	RIGHTDOWN = 0x40
+	RIGHTUP   = 0x80
 )
 
 func dumpBit(val uint64, i uint64) {
@@ -72,45 +85,22 @@ func dumpVerticalLines(lines [19]uint64) {
 
 
 func (game *GomokuBoard) dumpBoard() {
-//	dumpVerticalLines(game.VLines)
+	dumpVerticalLines(game.VLines)
+	dumpDiagonaleLines(game.DRLLines)
 	dumpDiagonaleLines(game.DLRLines)
-	//dumpDRLLines(game.DRLLines)
-	//dumpHorizontalLines(game.HLines)
+	dumpHorizontalLines(game.HLines)
 
 }
 
-func (game *GomokuBoard) setCaseHorizontal(line *uint64, x int, state uint64) {
-	if x == 18 {
-		*line |= state
-	} else {
-		
-		*line |= (state << (36 - uint64(2 * x)))
-	}
-}
 
-func (game *GomokuBoard) setCaseVertical(line *uint64, y int, state uint64) {
-	if y == 18 {
-		*line |= state
-	} else {
-		
-		*line |= (state << (36 - uint64(2 * y)))
-	}
-}
-
-
-func (game *GomokuBoard) setCaseDiagonaleTopBottom(line *uint64, x, y, index int, state uint64) {
-	if index <= 18 {
-		*line |= (state << (36 - uint64(2 * y)))
-	} else {
-		*line |= (state << uint64(2 * x))
-	}
-}
-
-func (game *GomokuBoard) setCaseDiagonaleBottomTop(line *uint64, x, y, index int, state uint64) {
-	if index <= 18 {
-		*line |= (state << (36 - uint64(2 * y)))
-	} else {
-		*line |= (state << (36 - uint64(2 * x)))
+func (game *GomokuBoard) clearBoard() {
+	for i := 0; i < 37; i++ {
+		if i < 19 {
+			game.HLines[i] &= 0
+			game.VLines[i] &= 0
+		}
+		game.DRLLines[i] &= 0
+		game.DLRLines[i] &= 0
 	}
 }
 
@@ -124,24 +114,187 @@ func (game *GomokuBoard) whatIsOnCase(x, y int) uint64 {
 	return EMPTY
 }
 
-func (game *GomokuBoard) emptyCaseHorizontal(line *uint64, x, y int) {
-	*line &= ^(game.whatIsOnCase(x, y) << (36 - uint64(2 * x)))
+// Il semblerait qu'en Go, ce qu'on fait dans d'autre langages avec l'operateur NOT (~) se fait avc '^'...
+func (game *GomokuBoard) emptyCase(x, y int) {
+	color := game.whatIsOnCase(x, y)
+
+	game.HLines[y] &= ^(color << (36 - uint64(2 * x)))
+
+	game.VLines[x] &= ^(color << (36 - uint64(2 * x)))
+
+	// Setting case on the diagonales lines (left to right and right to left)
+	indexDRL := x + y
+	if indexDRL <= 18 {
+		game.DRLLines[indexDRL] &= ^(color << (36 - uint64(2 * y)))
+	} else {
+		game.DRLLines[indexDRL] &= ^(color << uint64(2 * x))
+	}
+
+	indexDLR := 18 - (y - x)
+	if indexDLR >= 18 {
+		game.DLRLines[indexDLR] &= (color << (36 - uint64(2 * y)))
+	} else {
+		game.DLRLines[indexDLR] |= (color << (36 - uint64(2 * x)))
+	}
+
 }
 
-func (game *GomokuBoard) emptyCase(x, y int) {
-	game.emptyCaseHorizontal(&(game.HLines[y]), x, y)
-	//game.emptyCaseVertical(&(game.VLines[x]), y)
-	//game.emptyCaseDiagonaleTopBottom(&(game.DRLLines[index]), x, y, index)
-	/*if x >= y {
-		game.emptyCaseDiagonaleBottomTop(&(game.DLRLines[18 - (x - y)]), x, y, 18 - (x - y))
-	} else {
-		game.emptyCaseDiagonaleBottomTop(&(game.DLRLines[18 + (y - x)]), x, y, 18 + (y - x))
-	}*/
+func (game *GomokuBoard) moveIsDTDiagonaleLRSafe(x, y int) bool {
+	return true
+}
+
+func (game *GomokuBoard) moveIsDTDiagonaleRLSafe(x, y int) bool {
+	return true
+
+}
+
+func (game *GomokuBoard) moveIsDTVerticallySafe(x, y int) bool {
+	return true
+
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheLeft(x, y int) bool {
+	return (x >= 3 &&
+		game.whatIsOnCase(x - 1, y) == game.playerTurn &&
+		game.whatIsOnCase(x - 2, y) == game.playerTurn &&
+		game.whatIsOnCase(x - 3, y) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheRight(x, y int) bool {
+	return (x <= 15 &&
+		game.whatIsOnCase(x + 1, y) == game.playerTurn &&
+		game.whatIsOnCase(x + 2, y) == game.playerTurn &&
+		game.whatIsOnCase(x + 3, y) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheDown(x, y int) bool {
+	return (y <= 15 &&
+		game.whatIsOnCase(x, y + 1) == game.playerTurn &&
+		game.whatIsOnCase(x, y + 2) == game.playerTurn &&
+		game.whatIsOnCase(x, y + 3) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheUp(x, y int) bool {
+	return (y >= 3 &&
+		game.whatIsOnCase(x, y - 1) == game.playerTurn &&
+		game.whatIsOnCase(x, y - 2) == game.playerTurn &&
+		game.whatIsOnCase(x, y - 3) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheLeftUp(x, y int) bool {
+		return (x >= 3 &&
+		y >= 3 &&
+		game.whatIsOnCase(x - 1, y - 1) == game.playerTurn &&
+		game.whatIsOnCase(x - 2, y - 2) == game.playerTurn &&
+		game.whatIsOnCase(x - 3, y - 3) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheLeftDown(x, y int) bool {
+	return (x >= 3 && y <= 15 &&
+		game.whatIsOnCase(x - 1, y + 1) == game.playerTurn &&
+		game.whatIsOnCase(x - 2, y + 2) == game.playerTurn &&
+		game.whatIsOnCase(x - 3, y + 3) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheRightDown(x, y int) bool {
+	return (x <= 15 && y <= 15 &&
+		game.whatIsOnCase(x + 1, y + 1) == game.playerTurn &&
+		game.whatIsOnCase(x + 2, y + 2) == game.playerTurn &&
+		game.whatIsOnCase(x + 3, y + 3) == EMPTY)
+}
+
+func (game *GomokuBoard) checkDoubleThreeOnTheRightUp(x, y int) bool {
+	return (x <= 15 && y >= 3 &&
+		game.whatIsOnCase(x + 1, y - 1) == game.playerTurn &&
+		game.whatIsOnCase(x + 2, y - 2) == game.playerTurn &&
+		game.whatIsOnCase(x + 3, y - 3) == EMPTY)
+}
+
+func (game *GomokuBoard) initializeDTcheckFuncs() {
+	game.DoubleThreeCheckFuncs = make(map[uint8]func (int, int) bool)
+
+
+	game.DoubleThreeCheckFuncs[LEFT]	  = func (x, y int) bool { return game.checkDoubleThreeOnTheLeft(x, y) }
+	game.DoubleThreeCheckFuncs[RIGHT]	  = func (x, y int) bool { return game.checkDoubleThreeOnTheRight(x, y) }
+	game.DoubleThreeCheckFuncs[UP]	          = func (x, y int) bool { return game.checkDoubleThreeOnTheUp(x, y) }
+	game.DoubleThreeCheckFuncs[DOWN]	  = func (x, y int) bool { return game.checkDoubleThreeOnTheDown(x, y) }
+	game.DoubleThreeCheckFuncs[LEFTUP]	  = func (x, y int) bool { return game.checkDoubleThreeOnTheLeftUp(x, y) }
+	game.DoubleThreeCheckFuncs[LEFTDOWN]      = func (x, y int) bool { return game.checkDoubleThreeOnTheLeftDown(x, y) }
+	game.DoubleThreeCheckFuncs[RIGHTDOWN]     = func (x, y int) bool { return game.checkDoubleThreeOnTheRightDown(x, y) }
+	game.DoubleThreeCheckFuncs[RIGHTUP]	  = func (x, y int) bool { return game.checkDoubleThreeOnTheRightUp(x, y) }
+}
+
+func (game *GomokuBoard) checkThreesAround(x, y int, checkFlags uint8) (Three bool) {
+
+	Three = false
+	for flag, checkFunc := range game.DoubleThreeCheckFuncs {
+		if checkFlags & flag != 0 {
+			Three = checkFunc(x, y)
+			if Three == true {
+				break
+			}
+		}
+	}
+	return
+}
+
+func (game *GomokuBoard) horizontalLeftDoubleThree(x, y int) bool {
+	if x >= 3 && game.whatIsOnCase(x - 1, y) == game.playerTurn &&
+		game.whatIsOnCase(x - 2, y) == game.playerTurn &&
+		game.whatIsOnCase(x - 3, y) == EMPTY {
+		// possible double free on the left : check nearly everything !
+		return (game.checkThreesAround(x, y, UP | LEFTUP | LEFTDOWN | DOWN | RIGHTDOWN | RIGHTUP)     ||
+			game.checkThreesAround(x - 1, y, UP | LEFTUP | LEFTDOWN | DOWN | RIGHTDOWN | RIGHTUP) ||
+			game.checkThreesAround(x - 2, y, UP | LEFTUP | LEFTDOWN | DOWN | RIGHTDOWN | RIGHTUP))
+	}
+
+	return false
+}
+
+func (game *GomokuBoard) checkHorizontalFirstCase(x, y int) bool {
+	if x >= 3 && game.horizontalLeftDoubleThree(x, y) {
+		return false
+	}
+	
+	return true
+}
+
+func (game *GomokuBoard) moveIsDTHorizontallySafe(x, y int) bool {
+	return game.checkHorizontalFirstCase(x, y)
+	
+	return true
+}
+
+func (game *GomokuBoard) isMoveDoubleThreeSafe(x, y int) bool {
+	return game.moveIsDTHorizontallySafe(x, y)
+	/*&&
+		game.moveIsDTVerticallySafe(x, y)   && 
+		game.moveIsDTDiagonaleLRSafe(x, y)  &&
+		game.moveIsDTDiagonaleRLSafe(x, y)) */
+}
+
+func (game *GomokuBoard) moveIsValid(x, y int) error {	
+	if game.whatIsOnCase(x, y) != EMPTY {
+		return errors.New("Invalid move (stone already on this position)")
+	}
+	if game.doubleThree == true {
+		if game.isMoveDoubleThreeSafe(x, y) == false {
+			return errors.New("Invalid move (creating a Double Three)")
+		}
+	}
+	return nil
+}
+
+func (game *GomokuBoard) Play(x, y int) (int, [][2]int, error) {
+	err := game.moveIsValid(x, y)
+	if  err != nil {
+		return 0, nil, err
+	}
+
+	return 42, nil, nil
 }
 
 func (game *GomokuBoard) setCase(x, y int, state uint64) {
-	var index int
-	
 	// Setting case on horizontal lines
 	if x == 18 {
 		game.HLines[y] |= state
@@ -157,32 +310,26 @@ func (game *GomokuBoard) setCase(x, y int, state uint64) {
 		game.VLines[x] |= (state << (36 - uint64(2 * y)))
 	}
 
-	// Setting case on "right to left" diagonale lines
-	index = x + y
-	if index <= 18 {
-		game.DRLLines[index] |= (state << (36 - uint64(2 * y)))
+	// Setting case on the diagonales lines (left to right and right to left)
+	indexDRL := x + y
+	if indexDRL <= 18 {
+		game.DRLLines[indexDRL] |= (state << (36 - uint64(2 * y)))
 	} else {
-		game.DRLLines[index] |= (state << uint64(2 * x))
+		game.DRLLines[indexDRL] |= (state << uint64(2 * x))
 	}
 
-	// Setting case on "left to right" diagonale lines
-	if x >= y {
-		index = 18 - (x - y)
+	indexDLR := 18 - (y - x)
+	if indexDLR >= 18 {
+		game.DLRLines[indexDLR] |= (state << (36 - uint64(2 * y)))
 	} else {
-		index = 18 + (y - x)
-	}
-	if index <= 18 {
-		game.DLRLines[index] |= (state << (36 - uint64(2 * y)))
-	} else {
-		game.DLRLines[index] |= (state << (36 - uint64(2 * x)))
+		game.DLRLines[indexDLR] |= (state << (36 - uint64(2 * x)))
 	}
 }
 
 func main() {
 	var game GomokuBoard
 	
-	i := 0
-	tBefore := time.Now()
+/*	i := 0
 	for i < 19 {
 		j := 0
 		for j < 19 {
@@ -190,10 +337,27 @@ func main() {
 			j++
 		}
 		i++
-	}
-	fmt.Println("Le coup a pris", time.Since(tBefore))
-//	game.setCase(2, 3, 1)
-	game.emptyCase(1, 2)
+	}*/
+//	game.dumpBoard()
+//	game.clearBoard()
+// UP | LEFTUP | LEFTDOWN | DOWN | LEFTDOWN | LEFTUP
+	game.playerTurn = 1
+	game.initializeDTcheckFuncs()
+	game.setCase(9, 8, 1)
+	game.setCase(10, 7, 1)
+	game.setCase(8, 9, 1)
+	game.setCase(9, 9, 1)
+	game.setCase(7, 9, 1)
+	game.setCase(1, 9, 2)
+//	game.setCase(7, 9, 2)
+	// NOTE POUR PLUS TARD : ON A TROUVE !
+	// SI game.HLines[9] & (MASQUE << (36 - uint64(2 * 9))) == MASQUE && game.HLines[9] & (ENEMYMASK << (36 - uint64(2 * 9))) != ENEMYMASK
+	// -> la voie est libre !
 	game.dumpBoard()
-	println("Case 1, 2 is ", game.whatIsOnCase(1, 2))
+	println("Case 9, 9 is", game.whatIsOnCase(9, 9), "is (10, 9) double three safe? ")
+	tBefore := time.Now()
+	res := game.isMoveDoubleThreeSafe(10, 9)
+	fmt.Println(res, "(perf:", time.Since(tBefore), ")")
+	fmt.Println("test", game.HLines[9] & (37 << (36 - uint64(2 * 9))), (37 << (36 - uint64(2 * 9))))
+	
 }
