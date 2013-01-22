84d624a04629c37cae79592a097ae56273cccbef
Louis Roché <louis@louisroche.net>
2013-01-22 15:13:17 +0100

    Modification du main pour pouvoir utiliser pprof. ./gomoku -cpuprofile=cputest.prof ; go tool pprof ./gomoku cputest.prof

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
 import "runtime"
+import "flag"
+import "runtime/pprof"
+import "os"
+import "log"
+import "fmt"
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			fmt.Println("samarchpas")
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	board_display()
 }
